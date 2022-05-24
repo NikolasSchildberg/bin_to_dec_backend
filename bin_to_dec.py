012cbe2253c58243ef7ee4b4bf57bccd694ce86b
Nikolas Schildberg <nikolas.schildberg@gmail.com>
2022-05-24 08:48:13 -0300

    Refactor bin_to_dec as a module with a function

--- bin_to_dec.py
+++ bin_to_dec.py
@@ -1,10 +1,30 @@
-# decimal input and preprocessing
-bits_str = input("Enter a binary: ")
-bits_list = [int(bit) for bit in bits_str]
+def bin_to_dec(bits_str):
+    """
+    Converts a string representing a binary number to its decimal representation.
+    
+    Important:
+        Expects a string as argument for the binary number.
+        Returns a string containing the decimal representation.
+    """
+    # check argument type
+    if type(bits_str) != str:
+        raise TypeError("bin_to_dec() expects a string as argument")
+    
+    # check for invalid bits
+    for bit in bits_str:
+        if bit not in '01':
+            raise ValueError('Binary argument must contain only "0"s and "1"s.')
 
-# store decimal quantity relative to each bit
-decs = [bit*(2**pos) for pos, bit in enumerate(bits_list)]
-# sum them all up
-decimal = sum(decs)
+    # extract bits from string
+    bits_list = [int(bit) for bit in bits_str]
 
-print("decimal: ",decimal)
+    # sort to convert from zero-th power to biggest power
+    bits_list.reverse()
+
+    # store decimal quantity relative to each bit
+    decs = [bit*(2**pos) for pos, bit in enumerate(bits_list)]
+    
+    # sum all bits (decimal) values
+    decimal = sum(decs)
+
+    return(str(decimal))
