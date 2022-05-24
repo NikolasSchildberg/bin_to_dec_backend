c1490eebe6eee29d684273986c292e0abfedfff3
Nikolas Schildberg <nikolas.schildberg@gmail.com>
2022-05-24 09:07:23 -0300

    Refactor dec_to_bin as module with function

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -1,20 +1,34 @@
-# decimal input
-dec = int(input("Enter a decimal"))
+def dec_to_bin(dec_str):
+    """
+    Converts a string representing a decimal number to its binary representation.
+    
+    Important:
+        Expects a string as argument for the decimal number.
+        Returns a string containing the binary representation.
+    """
+    # check argument type
+    if type(dec_str) != str:
+        raise TypeError("dec_to_bin() expects a string as argument")
 
-# auxiliary variables
-current = dec
-bits = []
+    # check for invalid digits
+    for char in dec_str:
+        if char not in '0123456789':
+            raise ValueError('Decimal argument must contain only decimal digits.')
 
-# storing each bit in bits list
-while (current > 1):
-    bits.append(current%2)
-    current = current//2
+    # auxiliary variables for conversion
+    current = int(dec_str)
+    bits = []
 
-bits.append(current) # last remainder, after loop
+    # calculating and storing each bit into "bits" list
+    while (current > 1):
+        bits.append(current%2)
+        current = current//2
 
-bits_str = ''
-le = len(bits)
-for i in range(le):
-    bits_str += str(bits[le-i-1])
+    # stores last remainder too, after loop
+    bits.append(current)
 
-print('Bits string:', bits_str)
+    bits.reverse()
+    bits = [str(bit) for bit in bits]
+    bin_str = ''.join(bits)
+
+    return bin_str
